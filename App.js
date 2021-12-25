810d6a732b15c590635101a5c5bf19c56fddb14d
ndianko dieng <ndianko86@hotmail.com>
2021-12-25 13:51:20 -0500

    feat: wqeryhrt5uyimn

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-import express from  'express';// utilise express
-import dotenv from "dotenv";
+const express =require('express');// utilise express
 const app = express(); //crée une instance de express dans app
 
 app.set('views', './views'); //defini le dossier pour les views
 app.set('view engine', 'ejs');//défini ejs comme outil pour utiliser les views
 app.use('/public', express.static('public'));
-dotenv.config()
-app.get('/', (req, res)=>{
-    res.render('index.ejs', {pageTitle: "Avipole-home"});
-});
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.get('/services', (req, res)=>{
     res.render('services.ejs', {pageTitle: "Avipole-services"});
@@ -25,6 +22,9 @@ app.get('/contact', (req, res)=>{
     res.render('contact.ejs', {pageTitle: "Avipole-Contact"});
 });
 
+app.get("/", (req, res)=>{
+    res.render("index.ejs", {pageTitle: "Avipole-home"});
+});
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT, ()=>{ //le serveur attend les requetes sur le port 3000
